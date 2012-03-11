1cb5b1b1f4e9a2992807272b984d871ef9323cb8
Ninerian <hallo@daniel-der-grosse.de>
2012-03-11 23:04:06 +0100

    Erste Version

---
+++ config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+require '/Library/Ruby/Gems/1.8/gems/compass-susy-plugin-0.9/lib/susy.rb'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
